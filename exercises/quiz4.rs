968210f4594448dd66c2e040096a4a56001c8e7d
Jaskeerart Singh <jaskee789@gmail.com>
2020-08-29 12:09:28 +0530

    exercises done

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -3,9 +3,13 @@
 // - Modules
 // - Macros
 
-// Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+
+macro_rules! my_macro {
+    ($message:expr)=>{
+        format!("{} {}","Hello",$message)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -20,4 +24,5 @@ mod tests {
     fn test_my_macro_goodbye() {
         assert_eq!(my_macro!("goodbye!"), "Hello goodbye!");
     }
+    
 }
--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -3,7 +3,6 @@
 // exercise we have defined the Package struct and we want to test some logic attached to it,
 // make the code compile and the tests pass! If you have issues execute `rustlings hint structs3`
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -15,17 +14,22 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("less than zero")
         } else {
-            return Package {sender_country, recipient_country, weight_in_grams};
+            return Package {
+                sender_country,
+                recipient_country,
+                weight_in_grams,
+            };
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.recipient_country.ne(&self.sender_country)
     }
 
-    fn get_fees(&self, cents_per_kg: i32) -> ??? {
+    fn get_fees(&self, cents_per_kg: i32) -> i32 {
+        self.weight_in_grams * cents_per_kg / 1000
         // Something goes here... (beware of grams to kg conversion)
     }
 }
@@ -47,7 +51,7 @@ mod tests {
     fn create_international_package() {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Russia");
-        
+
         let package = Package::new(sender_country, recipient_country, 1200);
 
         assert!(package.is_international());
@@ -58,10 +62,10 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_kg = ???;
-        
+        let cents_per_kg = 3000;
+
         let package = Package::new(sender_country, recipient_country, 1500);
-        
+
         assert_eq!(package.get_fees(cents_per_kg), 4500);
     }
 }
